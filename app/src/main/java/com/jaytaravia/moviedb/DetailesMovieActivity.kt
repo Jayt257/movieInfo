c6d006cd0820860d4592f59ea6d250af19b84749
JayTaravia2002 <jaytaravia@gmail.com>
2023-02-12 13:29:01 +0530

    movie Info

---
+++ app/src/main/java/com/jaytaravia/moviedb/DetailesMovieActivity.kt
@@ -0,0 +1,11 @@
+package com.jaytaravia.moviedb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailesMovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailes_movie)
+    }
+}
---
+++ app/src/main/java/com/jaytaravia/moviedb/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.jaytaravia.moviedb.utils
+
+object Constants {
+
+    const val API_KEY=""
+    const val BASEURL=""
+    const val POSTER_BASEURL=""
+
+}
